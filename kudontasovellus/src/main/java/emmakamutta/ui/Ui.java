bc8fff2bbe647e77da6a8a956d1aced428d7008c
Emma Kamutta <emma.kamutta@helsinki.fi>
2020-11-24 11:23:10 +0200

    poistettu käyttöliittymä checkstylestä ja jacocosta

--- kudontasovellus/src/main/java/emmakamutta/ui/Ui.java
+++ kudontasovellus/src/main/java/emmakamutta/ui/Ui.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package emmakamutta.ui;
 
 /**
  *
- * @author Opiskelija-Emma
+ * Tänne tulee käyttöliittymä
  */
 public class Ui {
+
+                        public Ui(){
+    }
     
 }
