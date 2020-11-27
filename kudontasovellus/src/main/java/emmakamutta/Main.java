1644582c3b2e306bbfd3b339a2a7e3d1753ba4b4
Emma Kamutta <emma.kamutta@helsinki.fi>
2020-11-27 17:39:13 +0200

    javaFX viritelty

--- kudontasovellus/src/main/java/emmakamutta/Main.java
+++ kudontasovellus/src/main/java/emmakamutta/Main.java
@@ -2,11 +2,12 @@ package emmakamutta;
 
 
 import emmakamutta.domain.*;
+import emmakamutta.ui.Ui;
 
 
 
 public class Main {
     public static void main(String[] args) {
-      
+      Ui.main(args);
     }
 }
--- kudontasovellus/src/main/java/emmakamutta/ui/Ui.java
+++ kudontasovellus/src/main/java/emmakamutta/ui/Ui.java
@@ -1,13 +1,35 @@
 
 package emmakamutta.ui;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
 /**
  *
  * Tänne tulee käyttöliittymä
  */
-public class Ui {
+public class Ui extends Application{
 
-                        public Ui(){
+    public Ui(){
+    }
+
+    @Override
+    public void start(Stage ikkuna) throws Exception {
+           Button nappi = new Button("Tämä on nappi");
+
+        FlowPane komponenttiryhma = new FlowPane();
+        komponenttiryhma.getChildren().add(nappi);
+
+        Scene nakyma = new Scene(komponenttiryhma);
+
+        ikkuna.setScene(nakyma);
+        ikkuna.show();
     }
     
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
