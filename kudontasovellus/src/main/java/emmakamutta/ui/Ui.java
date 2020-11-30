9d2c2c3b073fc42f27ce22d35d0dade462369d5e
Emma Kamutta <emma.kamutta@helsinki.fi>
2020-11-30 15:54:04 +0200

    Käyttöliittymää työstetty

--- kudontasovellus/src/main/java/emmakamutta/ui/Ui.java
+++ kudontasovellus/src/main/java/emmakamutta/ui/Ui.java
@@ -2,9 +2,13 @@
 package emmakamutta.ui;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 /**
  *
@@ -14,18 +18,77 @@ public class Ui extends Application{
 
     public Ui(){
     }
+    
+    private Pane createNewDefaultLoom() {
+        
+        
+        return null;
+    }
+    
+    private Scene createDefaultScene() {
+        //Luodaan näkymä uudelle kudontamallille oletuskangaspuilla
+        GridPane weaveLayout = new GridPane();
+        weaveLayout.add(new Label("Näkymä vaihtui"), 1, 1);
+        weaveLayout.setPrefSize(300, 180);
+        weaveLayout.setAlignment(Pos.CENTER);
+        weaveLayout.setVgap(10);
+        weaveLayout.setHgap(10);
+        weaveLayout.setPadding(new Insets(20, 20, 20, 20));
+        
+        Scene defaultScene = new Scene(weaveLayout);
+        
+        return defaultScene;
+    }
+    
+    private Scene createCustomScene() {
+        //Luodaan näkymä uudelle kudontamallille itse määritellyillä kangaspuilla
+        GridPane weaveLayout = new GridPane();
+        weaveLayout.add(new Label("Tulossa pian :)"), 1, 1);
+        weaveLayout.setPrefSize(300, 180);
+        weaveLayout.setAlignment(Pos.CENTER);
+        weaveLayout.setVgap(10);
+        weaveLayout.setHgap(10);
+        weaveLayout.setPadding(new Insets(20, 20, 20, 20));
+        
+        Scene defaultScene = new Scene(weaveLayout);
+        
+        return defaultScene;
+    }
 
     @Override
-    public void start(Stage ikkuna) throws Exception {
-           Button nappi = new Button("Tämä on nappi");
+    public void start(Stage window) throws Exception {
+        window.setTitle("Kudontasovellus");
+        
+        //Luodaan ensin tervetulonäkymä
+        GridPane layout = new GridPane();
+        VBox menu = new VBox();
+        Button createNew = new Button("Uusi kudontamalli");
+        Button createCustom = new Button("Uusi kudontamalli omilla asetuksilla");
+        
+        menu.getChildren().addAll(createNew,createCustom);
+        layout.add(menu,1,1);
+        
+        layout.setPrefSize(300, 180);
+        layout.setAlignment(Pos.CENTER);
+        layout.setVgap(10);
+        layout.setHgap(10);
+        layout.setPadding(new Insets(20, 20, 20, 20));
 
-        FlowPane komponenttiryhma = new FlowPane();
-        komponenttiryhma.getChildren().add(nappi);
+        Scene welcome = new Scene(layout);
+        
+        
+        //Lisätään toiminnallisuudet tervetulonäkymän napeille
+        createNew.setOnAction( (event) ->{
+            window.setScene(createDefaultScene());
+        });
+        
+        createCustom.setOnAction( (event) ->{
+            window.setScene(createCustomScene());
+        });
+      
 
-        Scene nakyma = new Scene(komponenttiryhma);
-
-        ikkuna.setScene(nakyma);
-        ikkuna.show();
+        window.setScene(welcome);
+        window.show();
     }
     
     
